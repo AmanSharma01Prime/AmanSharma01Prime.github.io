a434129c07c7e3805d13c9891a762bcd37ff9a98
Aman Sharma <amansharma01prime@gmail.com>
2025-08-26 15:11:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+﻿// Scroll to section smoothly
+function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({ behavior: "smooth" });
+}
+
+// Highlight active nav link on scroll
+window.addEventListener("scroll", () => {
+  let sections = document.querySelectorAll("section");
+  let navLinks = document.querySelectorAll(".nav-links a");
+
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+        document
+          .querySelector(".nav-links a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+});
